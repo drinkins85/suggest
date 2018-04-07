aea4d4b9eb3d52a4696ee61a3f9d44fc6917b3fb
Andre <drinkins85@mail.ru>
2018-04-08 01:58:26 +0300

    Suggest

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import '../styles/main.scss';
 
-console.log('test');
+import PrefixTree from './PrefixTree';
+import streets from './data';
+
+const prefixTree = new PrefixTree();
+prefixTree.createFromArray(streets);
+
+const input = document.querySelector('.view-stub__input');
+const list = document.querySelector('.view-stub__list');
+
+input.addEventListener('focus', () => {
+  document.addEventListener('keyup', () => {
+    const searchText = input.value;
+    if (searchText.length > 2) {
+      let suggest = '';
+      prefixTree.suggest(input.value).forEach((item) => {
+        suggest += `<li class="suggest__item">${streets[item]}</li>`;
+      });
+      list.innerHTML = suggest;
+    }
+    if (searchText === '') {
+      list.innerHTML = '';
+    }
+  });
+});
+
